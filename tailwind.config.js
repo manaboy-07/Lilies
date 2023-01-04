a7d8f02c24d14e9381d40251212fd24fbd853c33
manaboy-07 <manassehoruebor@gmail.com>
2023-01-04 10:47:38 +0100

    finished setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
   content: [],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00302E",
+
+          secondary: "#FBDDBB",
+
+          accent: "#1FB2A6",
+
+          neutral: "#FFFFFF",
+
+          "base-100": "#2A303C",
+
+          info: "#3ABFF8",
+
+          success: "#3A8F4D",
+
+          warning: "#FBBD23",
+
+          error: "#C92C33",
+        },
+      },
+    ],
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
