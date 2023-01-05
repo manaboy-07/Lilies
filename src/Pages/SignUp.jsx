d5dbbe46df0fffceb15d7e1db032de9fcbd132f0
manaboy-07 <manassehoruebor@gmail.com>
2023-01-05 15:53:54 +0100

    working on signup page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+/** @format */
 
+import React from "react";
+import Register from "../Images/Register.png";
 function SignUp() {
   return (
-    <div>
-      
+    <div className='sign flex '>
+      <div>
+        <img src={Register} alt='' />
+      </div>
+      <div>
+        <form action=''>
+          <div className='input-container'>
+            <input type='text' placeholder='Your First Name' />
+          </div>
+          <div className='input-container'>
+            <input type='text' placeholder='Your Email address' />
+          </div>
+          <div className='input-container'>
+            <input type='text' placeholder='Your Password' />
+            <h3 className="text-primary">Show</h3>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
