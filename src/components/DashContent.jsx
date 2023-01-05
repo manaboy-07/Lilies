a092933f1fafad8996b9972e6dbd1a2f1163e2cf
manaboy-07 <manassehoruebor@gmail.com>
2023-01-05 18:45:51 +0100

    working on dashboard

--- src/components/DashContent.jsx
+++ src/components/DashContent.jsx
@@ -2,9 +2,13 @@
 
 import React from "react";
 import Soup from "../Images/soup.png";
+import DashProps from "./DashProps";
+import Spag from "../Images/HeroImage.png";
+import Burger from "../Images/burgers.png";
+import Stir from "../Images/Stir.png";
 function DashContent() {
   return (
-    <div className="bg-white dash-content">
+    <div className='bg-white dash-content'>
       <div className='p-4'>
         <section className='menu-div'>
           <div>
@@ -16,6 +20,10 @@ function DashContent() {
           </div>
         </section>
       </div>
+
+      <section>
+        <DashProps name='Burger Meal' src={Burger} des='Something sweet' />
+      </section>
     </div>
   );
 }
--- src/components/DashProps.jsx
+++ src/components/DashProps.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+/** @format */
 
-function DashProps() {
+import React from "react";
+
+function DashProps({ name, src, des }) {
   return (
-    <div>
-      
+    <div className='flex flex-col justify-center items-center my-4 border-2 border-slate-100 border-solid  p-3 rounded-md'>
+      <img src={src} alt='' className='w-36 h-36' />
+      <h3 className='text-black text-center text-2xl mt-2'>{name}</h3>
+      <h4 className='max-w-xl text-slate-500 mt-2'>{des}</h4>
+      <div className='prices flex justify-between items-center py-4 ml-2'>
+        <h2 className='text-black mx-2 font-extrabold'>₦1000.00</h2>
+        <h2 className='text-info mx-6 cursor-pointer'>Add to Cart</h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashProps
+export default DashProps;
